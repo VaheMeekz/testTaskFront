89e9735773584b225e5f042fd52c65749c792749
Vahe <71688163+VaheMeekz@users.noreply.github.com>
2022-04-28 13:39:23 +0400

    deploy fix

--- src/utils/keys.js
+++ src/utils/keys.js
@@ -8,4 +8,4 @@ export const API_URI = 'https://meekz007.herokuapp.com/api'
 
 export const token = localStorage.getItem("myToken")
 
-export const myUrl = "http://localhost:3000"
+export const myUrl = "https://test-task-front.vercel.app/"
